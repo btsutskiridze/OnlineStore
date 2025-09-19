f29fae087c77707d6c25e063fc693827be121d2c
btsutskirdze <baqaritsutskiridze222@gmail.com>
2025-09-20 00:31:47 +0400

    chore: remove unused services

--- src/Orders/Orders.Api/Services/IProductCatalogService.cs
+++
@@ -1,8 +0,0 @@
-namespace Orders.Api.Services
-{
-    public interface IProductCatalogService
-    {
-        Task<bool> DecrementStockAsync(int productId, int quantity, CancellationToken cancellationToken = default);
-        Task<bool> IsProductAvailableAsync(int productId, int quantity, CancellationToken cancellationToken = default);
-    }
-}
--- src/Orders/Orders.Api/Services/ITokenService.cs
+++
@@ -1,7 +0,0 @@
-namespace Orders.Api.Services
-{
-    public interface ITokenService
-    {
-        Task<string> GetServiceTokenAsync(string audience, CancellationToken cancellationToken = default);
-    }
-}
