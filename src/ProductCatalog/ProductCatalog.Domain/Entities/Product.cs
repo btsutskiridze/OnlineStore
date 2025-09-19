e72dd17a7ec19f442f3dddb1c0ccec306f7f09bd
btsutskirdze <baqaritsutskiridze222@gmail.com>
2025-09-20 02:30:05 +0400

    chore: update product entity

--- src/ProductCatalog/ProductCatalog.Domain/Entities/Product.cs
+++ src/ProductCatalog/ProductCatalog.Domain/Entities/Product.cs
@@ -3,10 +3,12 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string SKU { get; set; } = string.Empty;
+        public string Name { get; set; } = default!;
+        public string SKU { get; set; } = default!;
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
         public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
